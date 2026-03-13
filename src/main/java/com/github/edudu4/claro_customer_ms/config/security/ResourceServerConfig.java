15fe28455eceda5d5c30608f2a58571438edd437
Eduardo Alves da Rocha <eduardoa8142@gmail.com>
2026-03-13 06:14:26 -0300

    Corrigindo redundancia no security

--- src/main/java/com/github/edudu4/claro_customer_ms/config/security/ResourceServerConfig.java
+++ src/main/java/com/github/edudu4/claro_customer_ms/config/security/ResourceServerConfig.java
@@ -1,7 +1,6 @@
 package com.github.edudu4.claro_customer_ms.config.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -14,11 +13,16 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/oauth/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/h2-console/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/clientes/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/clientes/**").authenticated()
-                .antMatchers(HttpMethod.PUT, "/clientes/**").authenticated()
-                .antMatchers(HttpMethod.DELETE, "/clientes/**").authenticated()
-                .anyRequest().authenticated();
+                .antMatchers(
+                        "/oauth/**",
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/h2-console/**"
+                ).permitAll()
+                .antMatchers("/clientes/**").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .headers().frameOptions().disable();
     }
 }
--- src/main/java/com/github/edudu4/claro_customer_ms/config/security/SecurityConfig.java
+++ src/main/java/com/github/edudu4/claro_customer_ms/config/security/SecurityConfig.java
@@ -4,24 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/oauth/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/h2-console/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .headers().frameOptions().disable();
-    }
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
