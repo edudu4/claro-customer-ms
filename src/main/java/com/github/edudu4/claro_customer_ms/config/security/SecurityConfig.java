da62d89d5477f0795f02697876edada4efb2c328
Eduardo Alves da Rocha <eduardoa8142@gmail.com>
2026-03-13 16:55:11 -0300

    Correção do swagger no security

--- src/main/java/com/github/edudu4/claro_customer_ms/config/security/SecurityConfig.java
+++ src/main/java/com/github/edudu4/claro_customer_ms/config/security/SecurityConfig.java
@@ -4,12 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers(
+                "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html",
+                "/h2-console/**"
+        );
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
